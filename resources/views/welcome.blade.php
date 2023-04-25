e3366d95b0243d6117dc5afd5b7903d01eb34aea
n1k0nss <kostyantin.umsha@gmail.com>
2023-04-25 19:49:13 +0300

    #1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        @vite(['resources/scss/main/index.scss', 'resources/js/main/index.js'])
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
