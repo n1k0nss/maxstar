b47c4dbbd071fc4e50515a593f729a613ea81ba7
n1k0nss <kostyantin.umsha@gmail.com>
2023-04-26 17:55:03 +0300

    added controller and routes, fonts for main

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index(){
+        return view('main/index');
+    }
+
+    public function catalog(){
+        return view('catalog/index');
+    }
+
+    public function product(){
+        return view('product/index');
+    }
+}
---
+++ resources/views/main/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @vite(['resources/scss/main/index.scss', 'resources/js/main/index.js'])
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Maxstar</title>
+</head>
+<body>
+    @yield('content');
+</body>
+</html>
