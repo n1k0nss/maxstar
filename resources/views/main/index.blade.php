9f95c2ec74a8e779f72e32f9f14969c3e44d6860
n1k0nss <kostyantin.umsha@gmail.com>
2023-04-28 11:07:59 +0300

    added svg sprite, updated header

---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,6 @@
+@extends('main.master')
+
+@section('content')
+
+
+@endsection
--- resources/views/main/master.blade.php
+++ resources/views/main/master.blade.php
@@ -8,6 +8,64 @@
     <title>Maxstar</title>
 </head>
 <body>
-    @yield('content');
+    <header class="header">
+        <div class="header__container container">
+            <div class="header__logo">
+                <a href="logo__link link" href="#">
+                    <svg>
+                        <use href="./img/main/sprite.svg#icon-header-logo"></use>
+                    </svg>
+                </a>
+            </div>
+            <div class="header__search-modal">
+                <button class="header__search button" type="button" data-target="search-menu">
+                    <svg>
+                        <use href="./img/main/sprite.svg#icon-search"></use>
+                    </svg>
+                </button>
+                <div class="search-modal__box">
+                    <div class="modal__search">
+                        <form action="" class="modal__search-form">
+                            <input class="search-form__input" type="text" placeholder="Пошук товарів" name="search">
+                            <button class="search-form__button button" type="submit"></button>
+                        </form>
+                    </div>
+                    <div class="modal__contact">
+                        <div class="contact__title">
+                            <p>Не нашли что искали?</p>
+                            <p>Тогда позвоните нам по телефону</p>
+                        </div>
+                        <ul class="contact__number-list">
+                            <li class="number-list__item"><a href="tel:+380867870012">+38 086 787 00 12</a>
+                                 <svg>
+                                    <use href="./img/main/sprite.svg#icon-telegram-mini"></use>
+                                </svg>
+                            </li>
+                            <li class="number-list__item"><a href="tel:+380864577845">+38 086 457 78 45</a>
+                                <svg>
+                                   <use href="./img/main/sprite.svg#icon-whatsapp-mini"></use>
+                               </svg>
+                           </li>
+                           <li class="number-list__item"><a href="tel:+380864577845">+38 086 457 78 45</a>
+                            <svg>
+                               <use href="./img/main/sprite.svg#icon-viber-mini"></use>
+                           </svg>
+                       </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+            <div class="header__cart">
+
+            </div>
+            <div class="header__menu-dropdown">
+
+            </div>
+        </div>
+    </header>
+    <main>
+        @yield('content')
+    </main>
 </body>
 </html>
